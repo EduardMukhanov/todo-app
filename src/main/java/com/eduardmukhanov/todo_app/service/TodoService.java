7bde34c45bd4a0a19cae4d6cc77703fbe0f89025
Eduardik <emfo61933@gmail.com>
2026-01-04 21:56:09 +0300

    Добавил класс TodoService с CRUD-методами

---
+++ src/main/java/com/eduardmukhanov/todo_app/service/TodoService.java
@@ -0,0 +1,39 @@
+package com.eduardmukhanov.todo_app.service;
+
+import com.eduardmukhanov.todo_app.entity.Todo;
+import com.eduardmukhanov.todo_app.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    @Autowired
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    private final TodoRepository todoRepository;
+
+    public List<Todo> findAll(){
+        return todoRepository.findAll();
+    }
+
+    public Todo findById(Integer id){
+         return todoRepository.findById(id)
+                 .orElseThrow(() -> new RuntimeException("todo with id " + id + " not found")
+                 );
+    }
+
+    public Todo save(Todo todo){
+        return todoRepository.save(todo);
+    }
+
+    public void delete(Integer id){
+        if (!todoRepository.existsById(id)){
+            throw new RuntimeException("todo with id " + id + " not found");
+        }
+        todoRepository.deleteById(id);
+    }
+}
