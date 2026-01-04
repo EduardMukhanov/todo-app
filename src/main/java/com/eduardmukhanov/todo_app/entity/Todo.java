1b45e5c03690590264cea9b9bb2a969fb1535a45
Eduardik <emfo61933@gmail.com>
2026-01-04 22:31:53 +0300

    Добавил геттеры и сеттеры ко всем полям в классе Todo

--- src/main/java/com/eduardmukhanov/todo_app/entity/Todo.java
+++ src/main/java/com/eduardmukhanov/todo_app/entity/Todo.java
@@ -12,4 +12,36 @@ public class Todo {
     private String title;
     private Boolean status;
     private LocalDateTime createdAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
